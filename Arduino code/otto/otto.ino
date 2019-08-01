7c39dd5ac257f1c33dcd9550bdeec19e39788d97
Diego Costa <40510033+diegoserodio@users.noreply.github.com>
2019-08-01 13:56:11 -0300

    Add files via upload

--- Arduino code/otto/otto.ino
+++ Arduino code/otto/otto.ino
@@ -1,76 +1,77 @@
 #include <Servo.h>
 
-Servo quadrildireito;
-Servo quadrilesquerdo;
-Servo pedireito;
-Servo peesquerdo;
+Servo quadrildireito; //Quadril direito se refere ao servo 4.
+Servo quadrilesquerdo; //Quadril esquerdo se refere ao servo 2.
+Servo pedireito; //Pé direito se refere ao servo 3.
+Servo peesquerdo;//Pé esquerdo se refere ao servo 1.
 
-#define pin_quadril_direito 3
-#define pin_quadril_esquerdo 5
-#define pin_pe_direito 10
-#define pin_pe_esquerdo 11
+//Definido as entradas dos servos.
+#define pin_quadril_direito 2 
+#define pin_quadril_esquerdo 3
+#define pin_pe_direito 4
+#define pin_pe_esquerdo 5
 
 void setup() {
-  // put your setup code here, to run once:
- quadrildireito.attach(pin_quadril_direito);
- quadrilesquerdo.attach(pin_quadril_esquerdo);
- pedireito.attach(pin_pe_direito);
- peesquerdo.attach(pin_pe_esquerdo);
+  // Funções que serão rodadas apenas uma vez (aqui se inserem as funções báscias)
+  quadrildireito.attach(pin_quadril_direito);
+  quadrilesquerdo.attach(pin_quadril_esquerdo);
+  pedireito.attach(pin_pe_direito);
+  peesquerdo.attach(pin_pe_esquerdo);
 
 }
-
+//Funções ativas!
 void loop() {
-  andadinha();
-  plie();
+andadinha();
 }
-
-void giro (int angulo){
-quadrildireito.write(angulo);
-quadrilesquerdo.write(angulo);
-pedireito.write(angulo);
-peesquerdo.write(angulo);
+//Teste dos servos e função para rotação de TODOS os motores simultaneamente.
+void giro (int angulo) {
+  quadrildireito.write(angulo);
+  quadrilesquerdo.write(angulo);
+  pedireito.write(angulo);
+  peesquerdo.write(angulo);
 }
-
-void pedireitocima (){
+//Funções básicas para o pé direito.
+void pedireitocima () {
   quadrildireito.write(90);
   quadrilesquerdo.write(90);
-  pedireito.write(135);
+  pedireito.write(55);
 }
-void pedireitobaixo (){
+void pedireitobaixo () {
   quadrildireito.write(90);
   quadrilesquerdo.write(90);
   pedireito.write(90);
 }
-
-void peesquerdobaixo (){
+//Funções básicas pé esquerdo.
+void peesquerdocima () {
+  quadrildireito.write(90);
+  quadrilesquerdo.write(90);
+  peesquerdo.write(135);
+}
+void peesquerdobaixo () {
   quadrildireito.write(90);
   quadrilesquerdo.write(90);
   peesquerdo.write(90);
 }
 
-void peesquerdocima (){
-  quadrildireito.write(90);
-  quadrilesquerdo.write(90);
-  peesquerdo.write(55);
+//Dancinhas
+void andadinha () {
+giro(90);
+delay(1000);
+quadrildireito.write(55);
+pedireito.write(135);
+delay(1000);
+pedireito.write(90);
+delay(1000);
+quadrilesquerdo.write(90);
+peesquerdo.write(55);
+delay(1000);
+peesquerdo.write(90);
 }
 
-void andadinha (){
-  giro(90);
-delay(1000);
-pedireitocima();
-delay(1000);
-peesquerdocima();
-delay(1000);
-pedireitobaixo();
-delay(1000);
-peesquerdobaixo();
-}
-
-void pliet(){
+void pliet() {
   giro(90);
   delay(1000);
   pedireitocima();
   peesquerdocima();
   delay(1000);
 }
-
